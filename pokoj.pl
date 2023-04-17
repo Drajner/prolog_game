6971617f16c3d63ffa326882d343a0e032f96a30
Drajner <drajner@wp.pl>
2023-04-17 23:36:34 +0200

    added more interaction to pokoj

--- pokoj.pl
+++ pokoj.pl
@@ -9,18 +9,18 @@ zobacz(kalendarz) :- pozycja_gracza(pokoj),
                     write("Ledwo prowadził rakietę. Nie dotarł na narodziny swojego syna, chociaż i tak by tego nie pamiętał."), nl,
                     write("Dzień po pierwszym stycznia nie zatrzymał się w jego pamięci. Aldona nadal ma mu to za złe."), nl.
 
-zobacz(kuchnia) :- pozycja_gracza(pokoj),
+zobacz(kuchnia) :-  pozycja_gracza(pokoj),
                     write("Potworny syf rzucał się w oczy. Brudne naczynia leżały wszędzie."), nl.
 
 
-uzyj(kuchnia) :- pozycja_gracza(pokoj),
-                janusz_gotuje,
-                write("Kuchnia jest zajęta przez Janusza przygotowującego ryby na dzisiejszą kolacje."), nl.
+uzyj(kuchnia) :-    pozycja_gracza(pokoj),
+                    janusz_gotuje,
+                    write("Kuchnia jest zajęta przez Janusza przygotowującego ryby na dzisiejszą kolacje."), nl.
 
-uzyj(kuchnia) :- pozycja_gracza(pokoj),
-                write("Kapitanowi nie przystoi gotować na swoim statku, przecież ma od tego ludzi."), nl.
+uzyj(kuchnia) :-    pozycja_gracza(pokoj),
+                    write("Kapitanowi nie przystoi gotować na swoim statku, przecież ma od tego ludzi."), nl.
 
-uzyj(kuchnia) :- write("Nie ma tu kuchni, jeśli przez ostatnia godzine caly statek magicznie nie pozmienial swojego wyposazenia, to powinna nadal byc w pokoju."), nl.
+uzyj(kuchnia) :-    write("Nie ma tu kuchni, jeśli przez ostatnia godzine caly statek magicznie nie pozmienial swojego wyposazenia, to powinna nadal byc w pokoju."), nl.
 
 zobacz(skafander) :- klodka_zamknieta,
                     pozycja_gracza(pokoj),
@@ -39,7 +39,7 @@ zobacz(skafander) :- write("Gwiezdna Flota postanowiła wyposażyć statki „no
                     write("Gwiezdna Flota jak widać nie oszczędza na bezpieczeństwie swoich żołnierzy. Może i nie były tanie, ale za to były nędzne. Coś za coś."), nln
                     write("Miejmy nadzieję, że wyjście w tym poza statek nie będzie równało się pewnej śmierci."), nl,
 
-wpisz_kod(2137) :- klodka_zamknieta,
+wpisz_kod(2137) :-  klodka_zamknieta,
                     skafander_zbadany,
                     pozycja_gracza(pokoj),
                     write("„Nareszcie… 02.01.2037. Aldona nie mogłaś czegoś łatwiejszego wybrać?” - wyrzucił pod nosem Bomba otwierając zamek."), nl,
@@ -48,11 +48,29 @@ wpisz_kod(2137) :- klodka_zamknieta,
                     assert(przedmiot_w(skafander, pokoj)),
                     retractall(klodka_zamknieta).
 
-wpisz_kod(_) :- klodka_zamknieta,
-                skafander_zbadany,
-                pozycja_gracza(pokoj),
-                write("Pałąki kłódki nie puszczały niczym Mariusz Pudzianowski małego fiata w Siłaczach."), nl,
-                write("„To nie to… kiedy to było?” - zastanawiał się sfrustrowany kapitan."), nl.
+wpisz_kod(_) :-     klodka_zamknieta,
+                    skafander_zbadany,
+                    pozycja_gracza(pokoj),
+                    write("Pałąki kłódki nie puszczały niczym Mariusz Pudzianowski małego fiata w Siłaczach."), nl,
+                    write("„To nie to… kiedy to było?” - zastanawiał się sfrustrowany kapitan."), nl.
 
-wpisz_kod(_) :- write("Nie ma tu nic co mogłoby przyjąć kod."), nl.
+wpisz_kod(_) :-     write("Nie ma tu nic co mogłoby przyjąć kod."), nl.
+
+zobacz(szafki) :-   pozycja_gracza(pokoj),
+                    write("Szafki należące do Janusza i Seby były zamknięte na klucz. Zapewne każdy z nich ma swój klucz."), nl,
+                    write("[Jeśli kapitan dysponuje kluczem może skorzystać z komendy otworz(szafka_janusza) lub otworz(szafka_seby).]"), nl.
+
+otworz(szafka_seby) :- pozycja_gracza(pokoj),
+                    write("Szafka Seby niestety jest zamknięta i bez klucza może być niemożliwa do otwarcia."), nl.
+
+otworz(szafka_janusza) :- pozycja_gracza(pokoj),
+                    przedmiot_w(klucz_do_szafki_janusza, ekwipunek),
+                    write("Po krótkiej, lecz uciążliwej szamotaninie z przyrdzewiałym zamkiem szafki, Bombie udało się otworzyć szafkę podkomendnego."), nl,
+                    write("Niestety potyczka poskutkowała poważnymi stratami, klucz utknął w drzwiczkach i za nic nie chce ich opuścić."), nl,
+                    write("Wewnątrz znajduje się wedka_bez_haczyka oraz zolta_przyneta, zielona_przyneta, niebieska_przyneta. Niestety wędka może być problematyczna w użyciu bez haczyka."), nl,
+                    retract(przedmiot_w(klucz_do_szafki_janusza, ekwipunek))
+                    assert(przedmiot_w(wedka_bez_haczyka, pokoj)),
+                    assert(przedmiot_w(zolta_przyneta, pokoj)),
+                    assert(przedmiot_w(zielona_przyneta, pokoj)),
+                    assert(przedmiot_w(niebieska_przyneta pokoj)).
 
