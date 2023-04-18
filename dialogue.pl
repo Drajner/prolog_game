b4d9f04fba127a071e551643191f7bff7ec2e6df
Drajner <drajner@wp.pl>
2023-04-18 02:26:26 +0200

    expanded dialogue

--- dialogue.pl
+++ dialogue.pl
@@ -43,5 +43,28 @@ rozmawiaj(janusz, skafander) :- not(w_skafandrze), pozycja_gracza(pokoj),
                             write("„Janusz co wiesz o tym skafandrze?” - zapytał Bomba."), nl,
                             write("„Tak naprawdę to nie wiele.” - powiedział Seba."), nl,
                             write("„Hmm…”"), nl.
-rozmawiaj(janusz, chuda_ryba) :- not(w_skafandrze), pozycja_gracza(pokoj),
-rozmawiaj(janusz, przynety) :- not(w_skafandrze), pozycja_gracza(pokoj),
+rozmawiaj(janusz, chuda_ryba) :- not(w_skafandrze), pozycja_gracza(pokoj), przedmiot_w(chuda_ryba, ekwipunek)
+                            write("„Janusz co możesz mi powiedzieć o tej rybie?” - zapytał Bomba."), nl,
+                            write("„To bardzo ciekawy okaz. Czytałem kiedyś w czasopiśmie, że samce i samice tego gatunku bardzo się od siebie różnią."), nl,
+                            write("Samce są małe, chuderlawe i pełne ości. Samice za to są grube i pełne mięsa. Taką samicą to by się człowiek najadł…"), nl,
+                            write("I to nie jeden! Jakby pan jakąś znalazł, to mielibyśmy pyszną kolację."), nl,
+                            write("Zakładam, że jeśli jest tu samiec to pewnie i samica by się znalazła, bo te stworzenia zazwyczaj są spotykane w parach.” - opowiadał Janusz."), nl.
+
+rozmawiaj(janusz, wedka_bez_haczyka) :- not(w_skafandrze), pozycja_gracza(pokoj), szafka_janusza_otwarta,
+
+rozmawiaj(janusz, przynety) :- not(w_skafandrze), pozycja_gracza(pokoj), szafka_janusza_otwarta,
+
+
+
+rozmawiaj(janusz, gruba_ryba) :- not(w_skafandrze), pozycja_gracza(pokoj), przedmiot_w(gruba_ryba, ekwipunek), przedmiot_w(chuda_ryba, ekwipunek),
+                            write("„Cóż za piękna ryba!” - odparł Janusz po wzięciu samicy do rąk. - „Od razu zabieram się do kuchni."), nl,
+                            write("Jezu jaki ja jestem głodny! Niech mi pan da i tę drugą, będzie na przystawkę.”"), nl,
+                            write("Kapitan przekazał żołnierzowi dwie ryby, a ten prędko przeniósł się do kuchni."), nl,
+                            write("Ryby były abyt zajęte kłótnią pomiędzy sobą, żeby odnotować zmianę.")
+                            write("Na siedzeniu, które do niedawna zajmował, znajdował się śrubokręt."), nl,
+                            write("[Ryby zostały oddane, do ekwipunku dodano srubokret.]")
+                            assert(przedmiot_w(srubokret, ekwipunek)),
+                            retract(przedmiot_w(gruba_ryba, ekwipunek)),
+                            retract(przedmiot_w(chuda_ryba, ekwipunek)),
+                            assert(janusz_gotuje).
+
