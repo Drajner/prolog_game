945588c9a2e2d98b8751d733683b094751a28226
Aleksandra Majewska <amajewska4822@gmail.com>
2023-04-18 22:50:24 +0200

    adding descriptions of tail

--- descriptions.pl
+++ descriptions.pl
@@ -13,7 +13,7 @@ opis :- pozycja_gracza(pokoj), not(klodka_zamknieta), janusz_nie_gotuje,
         write("Przy stole w pokoju pogrywajac w karty siedza Janusz i Seba. Na scianie obskurnie wygladajacego pokoju bedacego centralnym pomieszczeniem Orzela 1"), nl,
         write("dumnie wisi kalendarz z rozneglizowana kobieta. Na prawej stronie znajdowala sie maleńka kuchnia. Przy wejsciu na ogon znajduja sie szafki nalezace do szeregowych."), nl,
         znajdz_przedmioty(pokoj),!.
-        
+
 opis :- pozycja_gracza(skrzydlo_lewe),
         write("Lewe skrzydlo ma na sobie jeden stary silnik."),nl,
         znajdz_przedmioty(skrzydlo_lewe),!.
@@ -27,6 +27,36 @@ opis :- pozycja_gracza(sluza),
         write("Teraz jest jeszcze gorzej. Na szczescie panel kontrolny nadal dziala i mozna otworzyc sluze."), nl,
         write("[Aby otworzyc sluze wpisz otworz_sluze lub zamknij_sluze, aby zamknac.]"), nl,
         znajdz_przedmioty(sluza),!.
+
+opis :-
+        pozycja_gracza(przod_ogona),
+        write('Twoim oczom ukazuje się szara, lekko oświetlona, podłużna przestrzeń.'), nl,
+        write('Możesz iść na tył ogona.'), nl,
+        write('Możesz zobaczyć co jest na prawo lub na lewo.'), nl.
+
+opis :-
+        pozycja_gracza(tyl_ogona),
+
+opis :-
+        pozycja_gracza(tyl_ogona),
+        zamknieta_skrzynka_na_narzędzia,
+        write('Stoi tu niewielka skrzynka na narzędzia … albo raczej na narzędzie.
+Jej rozmiar pozostawia wiele wątpliwości. Jest zamknięta na nietypową kłódkę,
+są na niej trzy przyciski: <^>v chyba należy je wcisnąć w jakiejś sekwencji.
+Aby wpisać hasło napisz: haslo(skrzynka, [sekwencja])'), nl,
+        write('Oprócz tego mogę rozejrzeć się w prawo lub w lewo.'), nl.
+
+opis :-
+        pozycja_gracza(tyl_ogona),
+        \+zamknieta_skrzynka_na_narzędzia,
+        przedmiot_w(klucz-francuski, tyl_ogona),
+        write('W skrzynce jest klucz francuski (klucz-francuski).'), nl.
+
+opis :-
+        pozycja_gracza(tyl_ogona),
+        \+zamknieta_skrzynka_na_narzędzia,
+        \+przedmiot_w(klucz-francuski, tyl_ogona),
+        write('Już nic tutaj nie ma.'), nl.
 
 znajdz_przedmioty(X) :- przedmiot_w(Y, X),
                         write("Lezy tu "), write(Y), write("."), nl, fail,!.
